17aa082adf4c574c4d1adf4da4f3fbdd5330d653
Azahel Caballero <acaballero@capsonic.local>
2019-09-13 12:54:42 -0600

    reads from file

--- cpc/Program.cs
+++ cpc/Program.cs
@@ -10,6 +10,10 @@ using Microsoft.Extensions.Configuration;
 using ServiceStack;
 using cpc.ServiceInterface;
 using cpc.ServiceModel;
+using ServiceStack.Configuration;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
 
 namespace cpc
 {
@@ -17,6 +21,61 @@ namespace cpc
     {
         public static void Main(string[] args)
         {
+
+
+            var sauce = @"C:\papercut-print-log-2019-05-03.csv";
+            if (!File.Exists(sauce)) throw new Exception("Error. File not found.");
+
+            var file = new List<List<string>>();
+            List<string> fileHeader;
+
+            #region Attempt Read from Txt File
+            //log("Intentando leer archivo: " + ev.FullPath);
+            file = null;
+
+            file = (from l in File.ReadLines(sauce, Encoding.GetEncoding("iso-8859-1"))
+                    let x = l.Split(new[] { ',' }).ToList()
+                    select x).ToList();
+            //log("Archivo leído correctamente.");
+
+            if (file.Count() <= 1)
+            {
+                //log("Archivo vacio.");
+                throw new Exception("Error. Empty File.");
+            }
+
+            #endregion
+
+            //log("Intentando leer encabezado");
+            #region Find Header Columns
+
+            var startLineNumber = file.FindIndex(l => l[0].Contains("Time"));
+            fileHeader = file[startLineNumber];
+            if (fileHeader == null)
+            {
+                //log("No se ha podido leer encabezado.");
+                throw new Exception("Error. Could not read Header.");
+            }
+
+
+
+            //log("Leyendo ubicación de columnas en encabezado");
+            Dictionary<string, int> header = new Dictionary<string, int>();
+            for (int i = 0; i < fileHeader.Count; i++)
+            {
+                //Omit repeated column titles.
+                try
+                {
+                    header.Add(fileHeader[i].ToLower(), i);
+                }
+                catch
+                {
+                    //log("Se omitió columna repetida: [" + fileHeader[i] + "]");
+                }
+            }
+
+            #endregion
+            /*
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
@@ -24,7 +83,9 @@ namespace cpc
                 .UseUrls(Environment.GetEnvironmentVariable("ASPNETCORE_URLS") ?? "http://localhost:5000/")
                 .Build();
 
+
             host.Run();
+            */
         }
     }
 
